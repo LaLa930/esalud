5e7ab93efca2019630582af7f781f2cb7199cdf2
Teresa Martínez <mar.2283@hotmail.com>
2020-06-01 20:52:30 +0200

    nada un espacio

--- src/routes/spoo.js
+++ src/routes/spoo.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Sensor = require('../models/spo2');
 const {isAuthenticated} = require ('../helpers/auth'); // para ver si esta autenticado o no
 
-// para etrar a la vista de tension
+
 router.get('/sensores/spoo', isAuthenticated , (req,res)=>{
   res.redirect('/spoo');
 });
